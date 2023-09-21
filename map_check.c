b706d07f50758718746bc9658236e85b6343c5ad
IremK <ayseirem.kayacioglu@gmail.com>
2023-09-21 11:46:54 +0300

    Add more details to the map check

--- map_check.c
+++ map_check.c
@@ -1,6 +1,31 @@
 #include "cub3d.h"
 
-int	char_check(t_map *Map, int i, int j)
+int	char_check2(t_map *Map, int i, int j)
+{
+	int zero_count;
+	int one_count;
+
+	zero_count = 0;
+	one_count = 0;
+	while(Map->map[++i])
+	{
+		j = -1;
+		while(Map->map[i][++j])
+		{
+			if (Map->map[i][j] == '0')
+				zero_count++;
+			else if (Map->map[i][j] == '1')
+				one_count++;
+		}
+	}
+	if (zero_count == 0)
+		return (err("No floors. Error\n"));
+	else if (one_count == 1)
+		return (err("No walls. Error\n"));
+	return (0);
+}
+
+int	char_check(t_map *Map, int i, int j) // To do: count 0s and 1s too and make sure there is at least one of each
 {
 	int	start_count;
 
@@ -26,7 +51,7 @@ int	char_check(t_map *Map, int i, int j)
 	}
 	if (start_count == 0)
 		return err("No starting position entered on map. Error\n");
-	return (0);
+	return (char_check2(Map, 5, -1));
 }
 
 int	color_check(char *color)
@@ -69,6 +94,12 @@ int wall_check(t_map *Map, int i, int j)
 	return (0);
 }
 
+int texture_check2(t_map *Map)
+{
+	return (!(file_check(Map->NO, 2) && file_check(Map->SO, 2)
+		&& file_check(Map->WE, 2) && file_check(Map->EA, 2)));
+}
+
 int texture_check(t_map *Map)
 {
 	// Texture adresses can be given in any order code below wont actually be enough for this project :(
@@ -82,4 +113,9 @@ int texture_check(t_map *Map)
 	//Check if the texture files exist, can be read, and have the right file ending
 	return (color_check(Map->F) || color_check(Map->C)
 			|| char_check(Map, 5, -1) || wall_check(Map, 5, -1));
+}
+
+int	map_check(t_map *Map)
+{
+	return (texture_check(Map) || char_check(Map, 5, -1) || wall_check(Map, 5, -1));
 }
